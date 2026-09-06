716766acbf3df38c1064021f14d6e4311791553d
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-06 20:12:35 +0200

    Add module loader

---
+++ src/WindowsDeviceLink/WindowsDeviceLink.psm1
@@ -0,0 +1,22 @@
+$privatePath = Join-Path $PSScriptRoot 'Private'
+$publicPath = Join-Path $PSScriptRoot 'Public'
+$script:BundledWindowsManagementServicePath = Join-Path $PSScriptRoot 'Runtime\Windows.Management.Service.dll'
+
+$nativeSource = Join-Path $privatePath 'DeviceLinkNative.cs'
+if (-not ('WinPEDeviceLink.Native.DeviceLinkClient' -as [type])) {
+    Add-Type -Path $nativeSource -ErrorAction Stop
+}
+
+Get-ChildItem -Path $privatePath -Filter '*.ps1' -File |
+    ForEach-Object { . $_.FullName }
+
+Get-ChildItem -Path $publicPath -Filter '*.ps1' -File |
+    ForEach-Object { . $_.FullName }
+
+Export-ModuleMember -Function @(
+    'Connect-WindowsDeviceLink'
+    'Export-WindowsDeviceLinkCsv'
+    'Get-WindowsDeviceLink'
+    'Register-WindowsDeviceLink'
+    'Test-WindowsDeviceLinkSupport'
+)
