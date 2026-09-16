e762ef703898b2b070fee938c2ca05ad2444c14e
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-16 21:09:43 +0200

    Sanitize webhook responses before returning them

--- src/WindowsDeviceLink/Private/Invoke-WindowsDeviceLinkWebhook.ps1
+++ src/WindowsDeviceLink/Private/Invoke-WindowsDeviceLinkWebhook.ps1
@@ -1,24 +1,13 @@
 function Invoke-WindowsDeviceLinkWebhook {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory)]
-        [PSTypeName('Windows.DeviceLink.Information')]
-        [psobject]$InputObject,
-
-        [Parameter(Mandatory)]
-        [ValidateNotNull()]
-        [uri]$WebhookUri,
-
-        [ValidateNotNullOrEmpty()]
-        [string]$WebhookApiKey,
-
-        [ValidateNotNullOrEmpty()]
-        [string]$TenantId
+        [Parameter(Mandatory)][PSTypeName('Windows.DeviceLink.Information')][psobject]$InputObject,
+        [Parameter(Mandatory)][ValidateNotNull()][uri]$WebhookUri,
+        [ValidateNotNullOrEmpty()][string]$WebhookApiKey,
+        [ValidateNotNullOrEmpty()][string]$TenantId
     )
 
-    [Net.ServicePointManager]::SecurityProtocol =
-        [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
     $module = Get-Module WindowsDeviceLink | Sort-Object Version -Descending | Select-Object -First 1
     $requestId = [guid]::NewGuid().ToString()
 
@@ -49,37 +38,17 @@ function Invoke-WindowsDeviceLinkWebhook {
         }
     }
 
-    $headers = @{
-        'X-WindowsDeviceLink-Schema' = '1'
-        'X-WindowsDeviceLink-RequestId' = $requestId
-    }
-
-    if ($PSBoundParameters.ContainsKey('WebhookApiKey')) {
-        $headers['X-WindowsDeviceLink-Key'] = $WebhookApiKey
-    }
+    $headers = @{'X-WindowsDeviceLink-Schema'='1';'X-WindowsDeviceLink-RequestId'=$requestId}
+    if ($PSBoundParameters.ContainsKey('WebhookApiKey')) { $headers['X-WindowsDeviceLink-Key'] = $WebhookApiKey }
 
     try {
         Write-Information -InformationAction Continue -MessageData "Sending DeviceLink payload to webhook. Request ID: $requestId"
-        $response = Invoke-RestMethod `
-            -Method POST `
-            -Uri $WebhookUri `
-            -Headers $headers `
-            -ContentType 'application/json' `
-            -Body ($payload | ConvertTo-Json -Depth 8 -Compress) `
-            -ErrorAction Stop
+        $response = Invoke-RestMethod -Method POST -Uri $WebhookUri -Headers $headers -ContentType 'application/json' -Body ($payload | ConvertTo-Json -Depth 8 -Compress) -ErrorAction Stop
     }
     catch {
         $statusCode = $null
-        try {
-            if ($_.Exception.Response -and $_.Exception.Response.StatusCode) {
-                $statusCode = [int]$_.Exception.Response.StatusCode
-            }
-        }
-        catch {}
-
-        $detail = Protect-WindowsDeviceLinkSensitiveText `
-            -Text ([string]$_.Exception.Message) `
-            -SensitiveValue @($WebhookApiKey, [string]$InputObject.DeviceLink)
+        try { if ($_.Exception.Response -and $_.Exception.Response.StatusCode) { $statusCode = [int]$_.Exception.Response.StatusCode } } catch {}
+        $detail = Protect-WindowsDeviceLinkSensitiveText -Text ([string]$_.Exception.Message) -SensitiveValue @($WebhookApiKey,[string]$InputObject.DeviceLink)
 
         switch ($statusCode) {
             400 { throw "DeviceLink webhook rejected the request with HTTP 400 (Bad Request). Request ID: $requestId. $detail" }
@@ -89,26 +58,20 @@ function Invoke-WindowsDeviceLinkWebhook {
             408 { throw "DeviceLink webhook request timed out (HTTP 408). Request ID: $requestId. $detail" }
             429 { throw "DeviceLink webhook is throttling requests (HTTP 429). Request ID: $requestId. $detail" }
         }
-
-        if ($statusCode -ge 500) {
-            throw "DeviceLink webhook returned server error HTTP $statusCode. Request ID: $requestId. $detail"
-        }
-        if ($statusCode) {
-            throw "DeviceLink webhook request failed with HTTP $statusCode. Request ID: $requestId. $detail"
-        }
-
+        if ($statusCode -ge 500) { throw "DeviceLink webhook returned server error HTTP $statusCode. Request ID: $requestId. $detail" }
+        if ($statusCode) { throw "DeviceLink webhook request failed with HTTP $statusCode. Request ID: $requestId. $detail" }
         throw "DeviceLink webhook could not be reached or the request failed before an HTTP response was received. Request ID: $requestId. $detail"
     }
-    finally {
-        $headers.Remove('X-WindowsDeviceLink-Key') | Out-Null
-    }
+    finally { $headers.Remove('X-WindowsDeviceLink-Key') | Out-Null }
+
+    $safeResponse = Protect-WindowsDeviceLinkObject -InputObject $response -SensitiveValue @($WebhookApiKey,[string]$InputObject.DeviceLink)
 
     [pscustomobject]@{
-        PSTypeName = 'Windows.DeviceLink.WebhookResult'
-        RequestId = $requestId
-        TenantId = $TenantId
-        SerialNumber = $InputObject.SerialNumber
-        WebhookUri = [string]$WebhookUri
-        Response = $response
+        PSTypeName='Windows.DeviceLink.WebhookResult'
+        RequestId=$requestId
+        TenantId=$TenantId
+        SerialNumber=$InputObject.SerialNumber
+        WebhookUri=[string]$WebhookUri
+        Response=$safeResponse
     }
 }
