07a7ecdeae4675ab837be4937b3cc5b6b73884a0
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-17 10:38:14 +0200

    Validate IID binary context research probe

--- tests/Research-Tools-Syntax-Validation.ps1
+++ tests/Research-Tools-Syntax-Validation.ps1
@@ -10,7 +10,8 @@ $ErrorActionPreference='Stop'
 $files=@(
     (Join-Path $PSScriptRoot '..\tools\Get-DeviceLinkRuntimeInventory.ps1'),
     (Join-Path $PSScriptRoot '..\tools\Get-DeviceLinkRuntimeRegistration.ps1'),
-    (Join-Path $PSScriptRoot '..\tools\Get-DeviceLinkCspInventory.ps1')
+    (Join-Path $PSScriptRoot '..\tools\Get-DeviceLinkCspInventory.ps1'),
+    (Join-Path $PSScriptRoot '..\tools\Get-DeviceLinkIidBinaryContext.ps1')
 )
 
 foreach($file in $files){
