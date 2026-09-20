55c81f7a6c18bc0439de74080fc4f3f605a8b7ec
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-20 09:29:23 +0200

    Allow delegated auth without explicit TenantId

--- src/WindowsDeviceLink/Public/Remove-WindowsDeviceLinkAssociation.ps1
+++ src/WindowsDeviceLink/Public/Remove-WindowsDeviceLinkAssociation.ps1
@@ -27,7 +27,6 @@ function Remove-WindowsDeviceLinkAssociation {
     if($invalidParameters){throw "The following parameters are not valid with -Method $Method`: $($invalidParameters -join ', ')."}
 
     switch($Method){
-        'Interactive'{if(-not $TenantId){throw '-TenantId is required for -Method Interactive.'}}
         'ClientSecret'{if(-not $TenantId -or -not $ClientId -or -not $PSBoundParameters.ContainsKey('ClientSecret')){throw '-TenantId, -ClientId, and -ClientSecret are required for -Method ClientSecret.'}}
         'AccessToken'{if(-not $TenantId -or -not $PSBoundParameters.ContainsKey('AccessToken')){throw '-TenantId and -AccessToken are required for -Method AccessToken.'}}
         'Certificate'{if(-not $TenantId -or -not $ClientId -or -not $Certificate){throw '-TenantId, -ClientId, and -Certificate are required for -Method Certificate.'}}
@@ -48,7 +47,7 @@ function Remove-WindowsDeviceLinkAssociation {
     } else {
         $connectParams=@{Environment=$Environment;ClientTimeout=$ClientTimeout}
         switch($Method){
-            'Interactive'{$connectParams.TenantId=$TenantId;if($ClientId){$connectParams.ClientId=$ClientId}}
+            'Interactive'{if($TenantId){$connectParams.TenantId=$TenantId};if($ClientId){$connectParams.ClientId=$ClientId}}
             'Certificate'{$connectParams.TenantId=$TenantId;$connectParams.ClientId=$ClientId;$connectParams.Certificate=$Certificate;$connectParams.SendCertificateChain=$SendCertificateChain}
             'CertificateThumbprint'{$connectParams.TenantId=$TenantId;$connectParams.ClientId=$ClientId;$connectParams.CertificateThumbprint=$CertificateThumbprint;$connectParams.SendCertificateChain=$SendCertificateChain}
             'CertificateSubjectName'{$connectParams.TenantId=$TenantId;$connectParams.ClientId=$ClientId;$connectParams.CertificateSubjectName=$CertificateSubjectName;$connectParams.SendCertificateChain=$SendCertificateChain}
