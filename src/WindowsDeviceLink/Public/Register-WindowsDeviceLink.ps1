6e23255bab9e362af77713ce4952e4505820cdcd
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-20 10:26:52 +0200

    feat: allow interactive registration without tenant id

--- src/WindowsDeviceLink/Public/Register-WindowsDeviceLink.ps1
+++ src/WindowsDeviceLink/Public/Register-WindowsDeviceLink.ps1
@@ -79,7 +79,6 @@ function Register-WindowsDeviceLink {
 
             switch ($Method) {
                 'DeviceCode' { if (-not $TenantId) { throw '-TenantId is required for -Method DeviceCode.' } }
-                'Interactive' { if (-not $TenantId) { throw '-TenantId is required for -Method Interactive.' } }
                 'ClientSecret' {
                     if (-not $TenantId -or -not $ClientId -or -not $PSBoundParameters.ContainsKey('ClientSecret')) {
                         throw '-TenantId, -ClientId, and -ClientSecret are required for -Method ClientSecret.'
@@ -167,7 +166,7 @@ function Register-WindowsDeviceLink {
             $connectParams = @{ Environment = $Environment; ClientTimeout = $ClientTimeout }
             switch ($Method) {
                 'Interactive' {
-                    $connectParams.TenantId = $TenantId
+                    if ($TenantId) { $connectParams.TenantId = $TenantId }
                     if ($ClientId) { $connectParams.ClientId = $ClientId }
                 }
                 'Certificate' {
