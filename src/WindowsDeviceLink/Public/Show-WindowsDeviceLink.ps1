957d24e8e46e4409e18d341be9d384407daf32e0
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-21 09:04:26 +0200

    refactor: use native confirmation dialogs in GUI

--- src/WindowsDeviceLink/Public/Show-WindowsDeviceLink.ps1
+++ src/WindowsDeviceLink/Public/Show-WindowsDeviceLink.ps1
@@ -498,64 +498,15 @@ function Show-WindowsDeviceLink {
             [string]$Message
         )
 
-        $dialog = New-Object System.Windows.Forms.Form
-        $dialog.Text = $Title
-        $dialog.StartPosition = 'CenterParent'
-        $dialog.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
-        $dialog.MaximizeBox = $false
-        $dialog.MinimizeBox = $false
-        $dialog.ShowInTaskbar = $false
-        $dialog.ClientSize = [System.Drawing.Size]::new(520,145)
-        $dialog.BackColor = [System.Drawing.Color]::White
+        $result = [System.Windows.Forms.MessageBox]::Show(
+            $form,
+            $Message,
+            $Title,
+            [System.Windows.Forms.MessageBoxButtons]::YesNo,
+            [System.Windows.Forms.MessageBoxIcon]::Warning
+        )
 
-        $iconBox = New-Object System.Windows.Forms.PictureBox
-        $iconBox.Location = [System.Drawing.Point]::new(20,24)
-        $iconBox.Size = [System.Drawing.Size]::new(34,34)
-        $iconBox.SizeMode = [System.Windows.Forms.PictureBoxSizeMode]::StretchImage
-        $iconBox.Image = [System.Drawing.SystemIcons]::Warning.ToBitmap()
-        $dialog.Controls.Add($iconBox)
-
-        $messageLabel = New-Object System.Windows.Forms.Label
-        $messageLabel.Text = $Message
-        $messageLabel.Font = New-Object System.Drawing.Font('Segoe UI',9)
-        $messageLabel.Location = [System.Drawing.Point]::new(68,20)
-        $messageLabel.Size = [System.Drawing.Size]::new(430,66)
-        $messageLabel.AutoEllipsis = $false
-        $dialog.Controls.Add($messageLabel)
-
-        $buttonPanel = New-Object System.Windows.Forms.Panel
-        $buttonPanel.Dock = [System.Windows.Forms.DockStyle]::Bottom
-        $buttonPanel.Height = 48
-        $buttonPanel.BackColor = [System.Drawing.Color]::FromArgb(246,246,246)
-        $dialog.Controls.Add($buttonPanel)
-
-        $yesButton = New-Object System.Windows.Forms.Button
-        $yesButton.Text = 'Yes'
-        $yesButton.DialogResult = [System.Windows.Forms.DialogResult]::Yes
-        $yesButton.Size = [System.Drawing.Size]::new(96,30)
-        $yesButton.Location = [System.Drawing.Point]::new(312,9)
-        $yesButton.Font = New-Object System.Drawing.Font('Segoe UI',9)
-        $buttonPanel.Controls.Add($yesButton)
-
-        $noButton = New-Object System.Windows.Forms.Button
-        $noButton.Text = 'No'
-        $noButton.DialogResult = [System.Windows.Forms.DialogResult]::No
-        $noButton.Size = [System.Drawing.Size]::new(96,30)
-        $noButton.Location = [System.Drawing.Point]::new(414,9)
-        $noButton.Font = New-Object System.Drawing.Font('Segoe UI',9)
-        $buttonPanel.Controls.Add($noButton)
-
-        $dialog.AcceptButton = $yesButton
-        $dialog.CancelButton = $noButton
-
-        try {
-            $result = $dialog.ShowDialog($form)
-            return ($result -eq [System.Windows.Forms.DialogResult]::Yes)
-        }
-        finally {
-            if ($iconBox.Image) { $iconBox.Image.Dispose() }
-            $dialog.Dispose()
-        }
+        return ($result -eq [System.Windows.Forms.DialogResult]::Yes)
     }
 
     function Set-GuiBusy {
